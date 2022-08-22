6fef594351184e0fbb0a743d8e5a412887e0f0d3
Samuel Majerus <ladolego@gmail.com>
2022-08-22 12:04:12 -0700

    End-of-'Wk4, Lsn 9'.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
+// Business Logic for AddressBook ---------
+function AddressBook() {
+  this.contacts = {};
+}
+
+AddressBook.prototype.addContact = function(contact) {
+  this.contacts[contact.firstName] = contact;
+};
+
+// Business Logic for Contacts ---------
 function Contact(firstName, lastName, phoneNumber) {
   this.firstName = firstName;
   this.lastName = lastName;
@@ -7,4 +17,3 @@ function Contact(firstName, lastName, phoneNumber) {
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName; 
 }; 
-
