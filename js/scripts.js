8a83a2600b0d5821660e7f5c0882c3008ef15c70
Samuel Majerus <ladolego@gmail.com>
2022-08-22 11:57:22 -0700

    End-of-'Wk4, Lsn 6'.

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+function Contact(firstName, lastName, phoneNumber) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.phoneNumber = phoneNumber;
+}
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName; 
+}; 
+
