4bc7336f16eecd251589bb80841dfea87cb52bfc
Samuel Majerus <ladolego@gmail.com>
2022-08-23 08:33:35 -0700

    My 'Wk 4, Lsn 18' code-along work.

--- js/scripts.js
+++ js/scripts.js
@@ -44,6 +44,20 @@ Contact.prototype.fullName = function() {
 // User Interface Logic ---------
 let addressBook = new AddressBook();
 
+function listContacts(addressBookToDisplay) {
+  let contactsDiv = document.querySelector("div#contacts");
+  contactsDiv.innerText =  null;
+  const ul = document.createElement("ul");
+  Object.keys(addressBookToDisplay.contacts).forEach(function(key) {
+    const contact = addressBookToDisplay.findContact(key);
+    const li = document.createElement("li");
+    li.append(contact.fullName());
+    li.setAttribute("id", contact.id);
+    ul.append(li);
+  });
+  contactsDiv.append(ul);
+}
+
 function handleFormSubmission(event) {
   event.preventDefault();
   const inputtedFirstName = document.querySelector("input#new-first-name").value;
@@ -51,7 +65,8 @@ function handleFormSubmission(event) {
   const inputtedPhoneNumber = document.querySelector("input#new-phone-number").value;
   let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
   addressBook.addContact(newContact);
-  console.log(addressBook.contacts);
+  //console.log(addressBook.contacts); 
+  listContacts(addressBook); 
 }
 
 window.addEventListener("load", function (){
