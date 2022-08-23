0ff802426130261202d3d08aa24a23d21fca6aef
Samuel Majerus <ladolego@gmail.com>
2022-08-22 22:57:56 -0700

    'Wk 4, Lsn 16' code-along work.

--- js/scripts.js
+++ js/scripts.js
@@ -40,3 +40,20 @@ Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName; 
 }; 
 
+
+// User Interface Logic ---------
+let addressBook = new AddressBook();
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  const inputtedFirstName = document.querySelector("input#new-first-name").value;
+  const inputtedLastName = document.querySelector("input#new-last-name").value;
+  const inputtedPhoneNumber = document.querySelector("input#new-phone-number").value;
+  let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
+  addressBook.addContact(newContact);
+  console.log(addressBook.contacts);
+}
+
+window.addEventListener("load", function (){
+  document.querySelector("form#new-contact").addEventListener("submit", handleFormSubmission);
+});
